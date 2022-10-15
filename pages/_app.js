f806c054176fc11ffb9779035c524951635d8de0
msanmaz <osanmaz4992@gmail.com>
2022-10-15 01:37:19 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {wrapper} from '../lib/store/index'
+import { Provider } from 'react-redux';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, ...rest }) {
+  const {store, props} = wrapper.useWrappedStore(rest);
+
+  return <Provider store={store}><Component {...props.pageProps} /></Provider>
 }
 
-export default MyApp
+export default MyApp;
